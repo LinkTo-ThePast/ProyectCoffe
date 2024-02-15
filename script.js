43b2f2d96f14e1a271f5f17b0b0d1d1ef7a028d9
LinkTo-ThePast <157771804+LinkTo-ThePast@users.noreply.github.com>
2024-02-15 04:38:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const nelsonButton = document.getElementById("nelsonButton");
+
+nelsonButton.addEventListener("click", () => {alert("Entiendo. Carita triste, carita triste, carita triste, carita triste. Échenme cal. ")}
+ );
+
+
+ 
+
+
